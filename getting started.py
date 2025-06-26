bc5ed3c8c69491dfb2d24a712784a634fc07be70
Denny Simanjuntak <dennysimanjuntak@Dennys-MacBook-Air.local>
2025-06-27 00:43:39 +0200

    Initial commit

---
+++ getting started.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Example plot
+df = sns.load_dataset("tips")
+sns.scatterplot(data=df, x="total_bill", y="tip")
+plt.show()
+√=cd ~/python-dev-project
+ls ~/python-dev-project
+
+source devenv/bin/activate
+streamlit run streamlit_app.py
+
